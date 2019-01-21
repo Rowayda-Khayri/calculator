c165af8de5e95c25469fbe784231a6a8fe9c9daa
Rowayda.Khayri <rowayda.khayri@gmail.com>
2019-01-21 18:12:53 +0200

    fix errors :D

--- wp-content/plugins/calculator/calculator.php
+++ wp-content/plugins/calculator/calculator.php
@@ -16,7 +16,7 @@ add_action('admin_menu','addMenu');
 
 function addMenu() {
     
-    add_menu_page('pageTitleCalculator', 'menuTitleCalculator', 4, 'Slugcalculator', 'calculate');
+    add_menu_page('pageTitleCalculator', 'menuTitleCalculator', 'read', 'Slugcalculator', 'calculate');
     
 }
 
@@ -25,7 +25,7 @@ function calculate() {
     echo 'Hi Calculator :)';
     
     ?>
-    <html>
+<!--    <html>
 	<head>
 		<title>Calculator</title>
 		<meta charset="utf-8">
@@ -34,15 +34,14 @@ function calculate() {
 		 
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 	</head>
-	<body>
+	<body>-->
 		
 		<div class="container" style="margin-top: 50px">
 		
 			<?php
 			
 				// If the submit button has been pressed
-				if(isset($_POST['submit']))
-				{
+				if ( isset( $_POST[ 'submit' ] ) ) {
 					// Check number values
 					if(is_numeric($_POST['number1']) && is_numeric($_POST['number2']))
 					{
@@ -65,7 +64,7 @@ function calculate() {
 						}
 						
 						// Print total to the browser
-//						echo "<h1>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total}</h1>";
+						echo "<h1>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total}</h1>";
 					
 					} else {
 						
@@ -78,7 +77,7 @@ function calculate() {
 			?>
 		    
 		    <!-- Calculator form -->
-		    <form method="post" action="calculator.php">
+		    <form method="post">
 		        <input name="number1" type="text" class="form-control" style="width: 150px; display: inline" />
 		        <select name="operation">
 		        	<option value="plus">Plus</option>
@@ -92,14 +91,10 @@ function calculate() {
 	    
 		</div>
 	
-	</body>
-</html>
+<!--	</body>
+</html>-->
 
 
 <?php
 }
-
-
-?>
-
 
